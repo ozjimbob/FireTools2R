8ce80c9611729709de440a1ab7a5a8b8b74cdbfd
Grant Williamson <Grant.Williamson@utas.edu.au>
2019-08-19 08:48:34 +1000

    Added function to write ESRI-acceptable projection information to GeoTIFF output, and included a projection file for EPSG 3308.

--- clean_output.r
+++ clean_output.r
@@ -75,16 +75,39 @@ rx_write=function(file,outfile){
   
 }
 
+# Insert 3308 projection into TIF output
+esri_output = function(tfile){
+  log_it("Generating ESRI projection")
+  infile = paste0(rast_temp,"/",tfile)
+  tempfile = paste0(rast_temp,"/",tfile,".tmp")
+  gt = Sys.which("gdal_translate")
+  cmd=paste0(gt," ",infile," -a_srs 3308.prj -co COMPRESS=LZW ",tempfile,intern=TRUE)
+  cout = system(cmd)
+  log_it(cout)
+  unlink(infile)
+  file.rename(tempfile,infile)
+}
+
+
 log_it("Writing heritage raster table")
 rx_write("r_vegout.tif","r_heritage_threshold_status.tif")
+esri_output("r_heritage_threshold_status.tif")
+
 log_it("Writing fmz raster table")
 rx_write("r_fmzout.tif","r_fmz_threshold_status.tif")
+esri_output("r_fmz_threshold_status.tif")
+
 log_it("Writing Heritage plus FMZ raster table")
 rx_write("r_fmz_bio_out.tif","r_heritage_fmz_threshold_status.tif")
+esri_output("r_heritage_fmz_threshold_status.tif")
+
 log_it("Writing FMZ plus SFAZ raster table")
 rx_write("r_sfaz_fmz_out.tif","r_fmz_sfaz_threshold_status.tif")
+esri_output("r_fmz_sfaz_threshold_status.tif")
+
 log_it("Writing combined raster table")
 rx_write("r_sfaz_fmz_bio_out.tif","r_heritage_fmz_sfaz_threshold_status.tif")
+esri_output("r_heritage_fmz_sfaz_threshold_status.tif")
 
 # Add table to vegetation map
 log_it("Adding labels to vegetation raster")
@@ -125,13 +148,26 @@ rat$ID = ids
 
 levels(tr) <- rat
 
+require(foreign)
+require(sp)
+atable = levels(tr)[[1]]
+names(atable)=c("VALUE","CATEGORY")
+x = as.data.frame(table(raster::values(tr)))
+names(x)=c("VALUE","COUNT")
+x$VALUE = as.numeric(as.character(x$VALUE))
+a2 = left_join(atable,x)
+a2$COUNT[is.na(a2$COUNT)]=0
+a2 = dplyr::select(a2,VALUE,COUNT,CATEGORY)
+write.dbf(a2,paste0(rast_temp,"/r_vegcode.tif.vat.dbf"))
+
+
 col_vec[1]="#ffffff"
 col_vec=c("#ffffff",col_vec)
 colortable(tr) <- col_vec
 
 log_it("Writing file")
 bigWrite(tr,paste0(rast_temp,"/r_vegcode2.tif"))
-
+esri_output("r_vegcode2.tif")
 
 log_it("Removing old files")
 unlink(paste0(rast_temp,"/r_vegcode.tif"))
@@ -144,8 +180,11 @@ file.rename(paste0(rast_temp,"/r_vegcode2.tif.aux.xml"),paste0(rast_temp,"/r_veg
 log_it("Renaming files")
 
 file.rename(paste0(rast_temp,"/rLastYearBurnt.tif"),paste0(rast_temp,"/r_LastYearBurnt.tif"))
+esri_output("r_LastYearBurnt.tif")
 file.rename(paste0(rast_temp,"/rNumTimesBurnt.tif"),paste0(rast_temp,"/r_NumTimesBurnt.tif"))
+esri_output("r_NumTimesBurnt.tif")
 file.rename(paste0(rast_temp,"/rTimeSinceLast.tif"),paste0(rast_temp,"/r_TimeSinceLast.tif"))
+esri_output("r_TimeSinceLast.tif")
 
 file.rename(paste0(rast_temp,"/v_vegout.gpkg"),paste0(rast_temp,"/v_heritage_threshold_status.gpkg"))
 file.rename(paste0(rast_temp,"/v_fmzout.gpkg"),paste0(rast_temp,"/v_fmz_threshold_status.gpkg"))
