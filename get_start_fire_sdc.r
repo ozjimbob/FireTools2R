217fa6a21225346cad7ac0d7661e547f4e4647dc
Grant Williamson <Grant.Williamson@utas.edu.au>
2022-04-08 09:03:04 +1000

    Altered SDC fire start to run from config file

--- get_start_fire_sdc.r
+++ get_start_fire_sdc.r
@@ -1,12 +1,18 @@
+#!/usr/bin/Rscript
+
 ## Get and process fire history
 library(tidyverse)
 library(sf)
 
-temp = "G:/temp_fd"
+args = commandArgs(trailingOnly=TRUE)
+source(args[1])
+
+
+#temp = "G:/temp_fd"
 dir.create(temp)
 lf <- list.files(temp)
 unlink(lf)
-download.file("https://datasets.seed.nsw.gov.au/dataset/1d05e145-80cb-4275-af9b-327a1536798d/resource/0674ee5b-033a-41ff-bb25-dd2242a9e093/download/fire_npwsfirehistory.zip",paste0(temp,"firehistory.zip"))
+download.file(fire_url,paste0(temp,"firehistory.zip"))
 
 unzip(paste0(temp,"firehistory.zip"),exdir=temp)
 
@@ -19,7 +25,7 @@ a <- a %>% mutate(year = case_when(month <= 6 ~ year-1,
 
 a$year[is.na(a$StartDate)] = substr(a$Label,1,4)[is.na(a$StartDate)]
 a$year <- as.character(a$year)
-keep ="G:/temp_fd/fire_history.gpkg"
+
 write_sf(a,keep)
 
 
@@ -27,20 +33,6 @@ this_year <- as.numeric(format(Sys.Date(),"%Y"))
 this_month <- as.numeric(format(Sys.Date(),"%m"))
 if(this_month >6){this_year = this_year+1}
 
-corp_gdb<-"../FireToolsSeverity/FESM_demo/FT_Input/Corporate.gdb"
-i_vt_boundary<-"NPWS_Branches"
-f_spatial_unit<-"NAME"
-d_spatial_unit<-""
-fire_gdb<-"G:/temp_fd/fire_history.gpkg"
-i_vt_fire_history<-"fire_history"
-f_fireseason<-"year"
-proj_crs<-"+proj=lcc +lat_1=-30.75 +lat_2=-35.75 +lat_0=-33.25 +lon_0=147 +x_0=9300000 +y_0=4500000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-ras_res<-250
-current_year<-this_year
-v_TSFF<-""
-future_years = 0
-subextent=NULL
-rast_temp<-"G:/fire_temp"
 
 passed=TRUE
 #cat(str,file=paste0(temp,"fire_config.r"))
