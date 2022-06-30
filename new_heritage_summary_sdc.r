eecef90ca4baf6e10c76569bc4e0d391bd6e0a04
Grant Williamson <Grant.Williamson@utas.edu.au>
2022-07-01 09:23:03 +1000

    Added SDC heritage summary logging

--- new_heritage_summary_sdc.r
+++ new_heritage_summary_sdc.r
@@ -12,8 +12,6 @@ library(furrr)
 source("../config/global_config.r")
 
 
-
-
 args <- commandArgs(trailingOnly=TRUE)
 source(args[1])
 
@@ -21,20 +19,28 @@ if(exists("sCPU")){
   clustNo <- sCPU
 }
 
+log_it = function(strn){
+  cat(paste0(Sys.time()," - ",strn),file=paste0(rast_temp,"/log.txt"),sep="\n",append=TRUE)
+  print(paste0("[FT2] ",Sys.time()," - ",strn))
+}
 
+log_it("Starting workers.")
 plan(multisession, workers = clustNo)
 
 # Make output folkders
+log_it(paste0("Output folder: ",rast_temp))
 dir.create(paste0(rast_temp,"/summary"))
 dir.create(paste0(rast_temp,"/metrics"))
 
 ## Overall summary table
-
+log_it("Overall summary table")
 year_list <- start_year:current_year
 
+log_it("Template raster")
 rl <- paste0(rast_temp,"/r_heritage_threshold_status_",year_list,".tif")
 vv <- rast(rl)
 
+log_it("Calculating frequencies")
 aa <- as_tibble(freq(vv))
 aa$layer<-year_list[aa$layer]
 rres <- res(vv)[1] * res(vv)[2]
@@ -58,7 +64,7 @@ aa$value   <- factor(aa$value ,labels=c("LongUnburnt","WithinThreshold",
                                                                            "Unknown","NoFireRegime"))
 
 rng <- aa %>% group_by(layer) %>% filter(!value %in% c("Unknown","NoFireRegime")) %>% summarise(area = sum(count)) %>% pull(area) %>% max()
-
+log_it("Making plot")
 pl <- ggplot(aa, aes(x=layer,fill=value ,y=count)) + 
   geom_bar(stat="identity",position = position_stack(reverse = TRUE)) + 
   theme_cowplot() + 
@@ -67,12 +73,15 @@ pl <- ggplot(aa, aes(x=layer,fill=value ,y=count)) +
   theme(axis.text.x=element_text(angle = -90, hjust = 0)) + 
   lims(y=c(0,rng))
 
+log_it("Saving plot")
 ggsave2(paste0(rast_temp,"/summary/area_ALL.pdf"),pl,width=1000,height=800,units="px",bg="white",scale=3)
 
 names(aa)<- c("Year","Status","Area")
 aa <- aa %>% pivot_wider(id_cols=c("Year"),names_from="Status",values_from = "Area")
 
 aa <- aa %>% rowwise() %>% mutate(Total = sum(c_across(!Year)))
+
+log_it("Saving CSV aerial summary")
 write_csv(aa,paste0(rast_temp,"/summary/area_ALL.csv"))
 
 # Now by veg type
@@ -96,11 +105,14 @@ crosstab_year <- function(i){
   ct
 }
 
+log_it("Area by formation")
+log_it("Starting crosstab")
 smr_form <- future_map_dfr(seq_along(year_list),crosstab_year)
 
 form_list <- unique(smr_form$Form)
-
+log_it("Making plots:")
 for(this_form in form_list){
+  log_it(paste0("Formation: ",this_form))
   this_smr <- filter(smr_form,Form==this_form)
   this_smr$Form <- NULL
   this_smr$Status <- factor(this_smr$Status ,labels=c("LongUnburnt","WithinThreshold",
@@ -131,6 +143,8 @@ for(this_form in form_list){
 }                     
 
 gc()
+
+log_it("Calculating landscape metrics")
 ### LANDSCAPE METRIC
 
 # Overall metrics
@@ -165,11 +179,12 @@ lm_all <- future_map_dfr(seq_along(year_list),lm_year)
 
 lm_all$class   <- factor(lm_all$class ,levels=c(1,2,3,4,5,9),labels=c("NoFireRegime","TooFrequentlyBurnt",
                                                                       "Vulnerable","LongUnburnt","WithinThreshold","Unknown"))
-
+log_it("Writing landscapemetrics")
 write_csv(lm_all,paste0(rast_temp,"/metrics/landscape_metrics_all.csv"))
 rm(lm_all)
 ### By formation - mask with terra
 
+log_it("Landscape metrics by formation")
 gc()
 form <- rast(paste0(rast_temp,"/veg/r_vegform.tif"))
 form_lut <- read_csv(paste0(rast_temp,"/form_lut.csv"))
@@ -206,20 +221,27 @@ lm_year_form <- function(i){
   bind_rows(list(output_landscape,output_class))
 }
 
+
 for(i in seq_along(unq_form_list)){
-  mask_tf = classify(form,rcl=cbind(unq_form_list[i],1),othersNA=TRUE)
+  
   this_form = form_lut$Form[form_lut$FormID==unq_form_list[i]]
+  log_it(paste0("Formation: ",this_form))
+  log_it("Masking")
+  mask_tf = classify(form,rcl=cbind(unq_form_list[i],1),othersNA=TRUE)
+  
+  log_it("Writing mask")
   writeRaster(mask_tf,paste0(rast_temp,"/veg/this_mask.tif"),overwrite=TRUE)
   print(this_form)
   # Overall metrics
   
-  
+  log_it("Running Landscape Metrics")
   lm_tf <- future_map_dfr(seq_along(year_list),lm_year_form,.progress=TRUE)
   unlink(paste0(rast_temp,"/veg/this_mask.tif"))
   lm_tf$formation = this_form
   out_lm_form[[i]]<-lm_tf
 }
 
+log_it("Writing formation landscape metrics")
 out_lm_form<- bind_rows(out_lm_form)
 
 out_lm_form$class   <- factor(out_lm_form$class ,levels=c(1,2,3,4,5,9),labels=c("NoFireRegime","TooFrequentlyBurnt",
@@ -230,18 +252,20 @@ rm(out_lm_form)
 gc()
 
 ## FESM OVERLAY
+log_it("FESM overlay")
 dir.create(paste0(rast_temp,"/fesm_overlay"))
 #fesm_dir <-"G:/ft_work/fesm/sdc_mosaics"
 
 fesm_statewide <- terra::rast(paste0(fesm_dir,"/fesm_overlay.vrt"))
 this_heritage <- rast(paste0(rast_temp,"/r_heritage_threshold_status_",year_list[length(year_list)],".tif"))
-
+log_it("Aligning extents")
 fst <- align(ext(fesm_statewide),this_heritage)
 ext(fesm_statewide)<-fst
 fesm_statewide = terra::crop(fesm_statewide,this_heritage)
 
 fesm_statewide = mask(fesm_statewide,vect(paste0(rast_temp,"/v_region.gpkg")))
 
+log_it("Writing clipped FESM")
 writeRaster(fesm_statewide,paste0(rast_temp,"/fesm_overlay/fesm_clip.tif"),overwrite=TRUE)
 fesm_statewide <- rast(paste0(rast_temp,"/fesm_overlay/fesm_clip.tif")) * 10
 
@@ -249,6 +273,7 @@ fesm_statewide[is.nan(fesm_statewide)]=0
 fesm_statewide[fesm_statewide %in% c(20,30)]=10
 fesm_statewide[fesm_statewide %in% c(40,50)]=20
 
+log_it("Mask and overlay")
 fesm_statewide = mask(fesm_statewide,vect(paste0(rast_temp,"/v_region.gpkg")))
 fesm_overlay <- fesm_statewide + this_heritage
 
@@ -274,8 +299,10 @@ col_table = data.frame(ID = c(0,
                                   "white","white","#4d0306","#4a2603","#065240","#4d4d4d","white","white","white","#49384f"))
 coltab(fesm_overlay) <- col_table$Status
 
+log_it("Writing map")
 bitmap(paste0(rast_temp,"/fesm_overlay/fesm_overlay.png"),width=1000,height=1300,units="px")
 plot(fesm_overlay)
 dev.off()
 
+log_it("Writing raster")
 terra::writeRaster(fesm_overlay,paste0(rast_temp,"/fesm_overlay/fesm_overlay.tif"),overwrite=TRUE)
